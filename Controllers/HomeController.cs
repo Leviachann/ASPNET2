102ba8a2e77a991a50eccf90fe1dc42a2596bc19
ScaraMouche <114559016+Leviachann@users.noreply.github.com>
2023-11-09 19:46:07 -0800

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Homework2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Homework2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿
+namespace Homework2.Controllers
+{
+    using Homework2.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>();
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product editedProduct)
+        {
+            var existingProduct = products.FirstOrDefault(p => p.Id == editedProduct.Id);
+            if (existingProduct != null)
+            {
+                existingProduct.Name = editedProduct.Name;
+                existingProduct.Description = editedProduct.Description;
+                existingProduct.Price = editedProduct.Price;
+                existingProduct.Discount = editedProduct.Discount;
+                existingProduct.PictureUrl = editedProduct.PictureUrl;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Remove(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product != null)
+            {
+                products.Remove(product);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product newProduct)
+        {
+            newProduct.Id = products.Count + 1; 
+            products.Add(newProduct);
+
+            return RedirectToAction("Index");
+        }
+    }
+
+}
